f2e1ba4490bf711ff8cb2cd7765dd1adbdc7b4b8
Anton Ivachev <drblack029@gmail.com>
2022-02-06 00:47:35 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -70,99 +70,36 @@ function App() {
 						id: 'asd_14112'
 					},
 					{
-						title: 'Title',
+						title: 'Title4',
 						description: 'Text',
 						id: '1231aqsda'
 					},
 					{
-						title: 'Title2',
+						title: 'Title5',
 						description: 'Text2',
 						id: 'asd_w131'
 					},
 					{
-						title: 'Title3',
+						title: 'Title6',
 						description: 'Text3',
 						id: 'asdqq_141'
 					},{
-						title: 'Title',
+						title: 'Title7',
 						description: 'Text',
 						id: 'aszaw2'
 					},
 					{
-						title: 'Title2',
+						title: 'Title8',
 						description: 'Text2',
 						id: 'as1ad_131'
 					},
 					{
-						title: 'Title3',
+						title: 'Title9',
 						description: 'Text3',
 						id: 'asd_11q41'
 					},
 				],
 			},
-			{
-				boardTitle: "Вопросы собеседование",
-				id: '1qqq123123',
-				cardsList: [
-					{
-						title: 'Title',
-						description: 'Text',
-						id: '2212312'
-					},
-					{
-						title: 'Title2',
-						description: 'Text2',
-						id: 'asd_131'
-					},
-					{
-						title: 'Title3',
-						description: 'Text3',
-						id: 'asdqq141'
-					},
-				],
-			},
-			{
-				boardTitle: "Вопросы собеседование",
-				id: '123123asda1',
-				cardsList: [
-					{
-						title: 'Title',
-						description: 'Text',
-						id: '12312341'
-					},
-					{
-						title: 'Title2',
-						description: 'Text2',
-						id: 'asd_1sa31'
-					},
-					{
-						title: 'Title3',
-						description: 'Text3',
-						id: 'asd_141'
-					},
-				],
-			},
-			{
-				boardTitle: "Вопросы собеседование",
-				id: '12sss213123',
-				cardsList: [
-					{
-						title: 'Title',
-						description: 'Text',
-						id: 'asd_121'
-					},
-					{
-						title: 'Title2',
-						description: 'Text2',
-						id: 'asd_131asdas'
-					},
-					{
-						title: 'Title3',
-						description: 'Text3',
-						id: 'aqqsd_141'
-					},
-				],
-			}
 	]
 	)
 
--- src/components/board-item/boardItem.tsx
+++ src/components/board-item/boardItem.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './style.module.css';
+import { globalStateContext } from '../../globalState/globalState';
 
 interface AllProps {
     title: string,
@@ -7,19 +8,8 @@ interface AllProps {
     id: string,
 }
 
-const dragStartHandler = (e, card) => {
+const dragStartHandler = (card) => {
     console.log('drag start')
-    console.log(card)
-}
-
-const dragEndHandler = (e, card) => {
-    // console.log('drag end')
-    // console.log(card)
-}
-
-const dropHandler = (e, card) => {
-    e.preventDefault()
-    console.log('drag drop')
     console.log(card)
 }
 
@@ -32,13 +22,44 @@ const dragLeaveHandler = (e) => {
 }
 
 export const BoardItem = (props: AllProps) => {
+
+    const [boards, setBoards] = useContext(globalStateContext)
+
+    const dropHandler = (e, card) => {
+        e.preventDefault()
+        console.log('drag drop')
+        // console.log(card.title + ' ' + card.id)
+    
+    }
+
+    const dragEndHandler = (card) => {
+        console.log('drag end')
+        console.log(card)
+
+        let boardsSort = [...boards]
+        
+        // boardsSort = boardsSort.map(e => e.cardsList.filter(item => item.id != card.id))
+
+        // console.log(boardsSort)
+
+        // boardsSort = boardsSort.map(e => {
+        //     return {e, ...e.cardsList.filter(item => item.id != card.id)}
+        // })
+
+        // boardsSort = boardsSort.map(e => Object.keys(e).filter(item => item.id != card.id))
+
+        // boardsSort.forEach(e => Object.keys(e).forEach(e => console.log(e)))
+
+        console.log(boardsSort)
+    }
+
     return (
         <div 
             className={styles.boardITemWrapper}
             draggable={true}
-            onDragStart={e => dragStartHandler(e, e.target)}
-            onDragEnd={e => dragEndHandler(e, e.target)}
-            onDrop={e => dropHandler(e, e.target)}
+            onDragStart={e => dragStartHandler(props)}
+            onDragEnd={e => dragEndHandler(props)}
+            onDrop={e => dropHandler(e, props)}
             onDragOver={e => dragOverHandler(e)}
             onDragLeave={e => dragLeaveHandler(e)}
         >
